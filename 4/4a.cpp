249f623d2762d1a07a49b201f532ff0b3d72e5cc
Siyuan Chen <siyuanc2@andrew.cmu.edu>
2022-12-04 11:46:37 -0500

    Day 4

--- 4/4a.cpp
+++ 4/4a.cpp
@@ -3,8 +3,29 @@
 using namespace std;
 
 int main() {
+    int cnt = 0;
     string line;
     while (getline(cin, line)) {
-        cout << line.size() << '\n';
+        int l1, r1, l2, r2;
+        int tmp;
+
+        tmp = line.find('-');
+        l1 = stoi(line.substr(0, tmp));
+        line = line.substr(tmp + 1);
+
+        tmp = line.find(',');
+        r1 = stoi(line.substr(0, tmp));
+        line = line.substr(tmp + 1);
+
+        tmp = line.find('-');
+        l2 = stoi(line.substr(0, tmp));
+        line = line.substr(tmp + 1);
+
+        r2 = stoi(line);
+
+        if (!(r1 < l2 || l1 > r2)) {
+            cnt++;
+        }
     }
+    cout << cnt << '\n';
 }
