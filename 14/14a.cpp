a06459bed29956be3942e34ac55066da514a6607
Siyuan Chen <siyuanc2@andrew.cmu.edu>
2022-12-14 20:33:27 -0500

    Day 14

--- 14/14a.cpp
+++ 14/14a.cpp
@@ -5,4 +5,60 @@ using ll = long long;
 
 int main() {
     ifstream fin("../14/14.txt");
+
+    int const DIMENSION = 600;
+    vector B(DIMENSION, vector<bool>(DIMENSION));
+
+    string line;
+    while (getline(fin, line)) {
+        int px = stoi(line);
+        line = line.substr(line.find(',') + 1);
+        int py = stoi(line);
+        while (true) {
+            string::size_type pos = line.find(" -> ");
+            if (pos == string::npos)
+                break;
+            line = line.substr(pos + 4);
+            int cx = stoi(line);
+            line = line.substr(line.find(',') + 1);
+            int cy = stoi(line);
+            for (int i = min(px, cx); i <= max(px, cx); i++)
+                for (int j = min(py, cy); j <= max(py, cy); j++) {
+                    assert(0 <= i && i < DIMENSION);
+                    assert(0 <= j && j < DIMENSION);
+                    B[i][j] = true;
+                }
+            px = cx;
+            py = cy;
+        }
+    }
+
+    int cnt = 0;
+    while (true) {
+        int x = 500;
+        int y = 0;
+        while (true) {
+            if (y == DIMENSION - 1) {
+                cout << cnt << '\n';
+                return 0;
+            }
+            if (!B[x][y + 1]) {
+                y++;
+                continue;
+            }
+            if (!B[x - 1][y + 1]) {
+                x--;
+                y++;
+                continue;
+            }
+            if (!B[x + 1][y + 1]) {
+                x++;
+                y++;
+                continue;
+            }
+            B[x][y] = true;
+            cnt++;
+            break;
+        }
+    }
 }
---
+++ 15/15a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ifstream fin("../15/15.txt");
+
+    string line;
+    while (getline(fin, line)) {
+    }
+}
