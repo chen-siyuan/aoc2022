c63c87a36dca981902a412e9082d2c2af5b38679
Siyuan Chen <siyuanc2@andrew.cmu.edu>
2022-12-18 00:42:51 -0500

    Day 18 incomplete :(

---
+++ 19/19a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    ifstream fin("../19/19.txt");
+
+    clock_t timer = clock();
+    cout << fixed << setprecision(6) << double(clock() - timer) / CLOCKS_PER_SEC << '\n';
+}
