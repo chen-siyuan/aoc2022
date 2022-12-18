ffb47ced55001f2b91a4b0036f7f17d7fa89d470
Siyuan Chen <siyuanc2@andrew.cmu.edu>
2022-12-18 00:41:18 -0500

    Day 17 incomplete :(

---
+++ 17/17b.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    // TODO: Find the right period
+    ifstream fin("../17/17.txt");
+
+    vector<pair<int, int> const> const ROCKS[5] = {
+            {{0, 0}, {1, 0}, {2, 0}, {3, 0}},
+            {{1, 0}, {0, 1}, {1, 1}, {2, 1}, {1, 2}},
+            {{0, 0}, {1, 0}, {2, 0}, {2, 1}, {2, 2}},
+            {{0, 0}, {0, 1}, {0, 2}, {0, 3}},
+            {{0, 0}, {1, 0}, {0, 1}, {1, 1}}
+    };
+    auto next_rock = [&]() -> vector<pair<int, int> const> const & {
+        static int i = 0;
+        return ROCKS[i++ % 5];
+    };
+
+    string pattern;
+    getline(fin, pattern);
+    auto next_push = [&]() -> int {
+        static int i = 0;
+        return pattern[i++ % int(pattern.size())] == '<' ? -1 : 1;
+    };
+
+    int const N = pattern.size() * 5 * 2;
+    vector<array<bool, 7>> chamber(N * 5);
+    auto is_empty = [&](int x, int y) -> bool {
+        return 0 <= x && x < 7 && 0 <= y && y < N * 5 && !chamber[y][x];
+    };
+
+    clock_t timer = clock();
+
+    int height = 0;
+    for (int i = 0; i < N; i++) {
+        auto rock = next_rock();
+
+        int x = 2, y = height + 3;
+        while (true) {
+            int push = next_push();
+            bool flag = true;
+            for (auto [dx, dy]: rock)
+                if (!is_empty(x + dx + push, y + dy))
+                    flag = false;
+            if (flag)
+                x += push;
+            flag = true;
+            for (auto [dx, dy]: rock)
+                if (!is_empty(x + dx, y + dy - 1))
+                    flag = false;
+            if (flag) y--;
+            else break;
+        }
+
+        for (auto [dx, dy]: rock)
+            chamber[y + dy][x + dx] = true;
+
+        while (true) {
+            bool flag = true;
+            for (int j = 0; j < 7; j++)
+                if (!is_empty(j, height))
+                    flag = false;
+            if (flag)
+                break;
+            height++;
+        }
+    }
+
+    cout << N << '\n';
+    cout << height << '\n';
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 7; j++)
+            cout << (chamber[9 - i][j] ? '#' : '.');
+        cout << '\n';
+    }
+
+    cout << '\n';
+    for (int i = 0; i < 4; i++)
+        cout << "......." << '\n';
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 7; j++)
+            cout << (chamber[height - 1 - i][j] ? '#' : '.');
+        cout << '\n';
+    }
+
+    timer = clock() - timer;
+    cout << fixed << setprecision(6) << double(timer) / CLOCKS_PER_SEC << '\n';
+}
---
+++ 18/18b.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int const N = 50;
+
+bool B[N][N][N];
+
+int main() {
+    // TODO: Calculate exterior surface area
+    ifstream fin("../18/18.txt");
+
+    clock_t timer = clock();
+
+    int res = 0;
+    auto check = [&](int x, int y, int z) -> void {
+        if (0 <= x && x < N && 0 <= y && y < N && 0 <= z && z < N && B[x][y][z]) res--;
+        else res++;
+    };
+    while (!fin.eof()) {
+        string line;
+        getline(fin, line);
+        int x = stoi(line);
+        line = line.substr(line.find(',') + 1);
+        int y = stoi(line);
+        line = line.substr(line.find(',') + 1);
+        int z = stoi(line);
+        assert(0 <= x && x < N && 0 <= y && y < N && 0 <= z && z < N);
+        check(x + 1, y, z);
+        check(x - 1, y, z);
+        check(x, y + 1, z);
+        check(x, y - 1, z);
+        check(x, y, z + 1);
+        check(x, y, z - 1);
+        B[x][y][z] = true;
+    }
+
+    cout << res << '\n';
+
+    cout << fixed << setprecision(6) << double(clock() - timer) / CLOCKS_PER_SEC << '\n';
+}
